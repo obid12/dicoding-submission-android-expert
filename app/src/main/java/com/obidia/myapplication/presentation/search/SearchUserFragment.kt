9c1068686cf5d153ff99d3a91fa896702bd701ac
obid12 <obidiaramdhani23@gmail.com>
2024-05-23 16:15:20 +0700

    try

--- app/src/main/java/com/obidia/myapplication/presentation/search/SearchUserFragment.kt
+++ app/src/main/java/com/obidia/myapplication/presentation/search/SearchUserFragment.kt
@@ -164,7 +164,7 @@ class SearchUserFragment : Fragment() {
     }
   }
 
-  private fun showContent(index: Int) {
+  private fun showContent(index: Int)  {
     binding.animator.displayedChild = index
   }
 
